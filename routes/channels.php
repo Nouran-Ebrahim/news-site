d9e679a7cc4294fa2a8df3947776d2343cda7f0a
Nouran El Mohamady <nouran.ssp@gmail.com>
2025-05-13 17:51:23 +0300

    edit

--- routes/channels.php
+++ routes/channels.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Broadcast;
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-//$user this is the auth user in guard web
+//$user this is the auth user in guard web that will resive the notification
     return (int) $user->id === (int) $id;
 });
 Broadcast::channel('App.Models.Admin.{id}', function ($user, $id) {
